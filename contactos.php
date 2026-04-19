476820be523e6c00f4d955dd574cb64f8733312d
gasela <franciscogoncalves008@gmail.com>
2026-04-19 15:31:57 +0100

    Update contactos.php

--- contactos.php
+++ contactos.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <!-- Menu de navegação -->
     <nav id="menu">
         <ul class="menu-lista">
-            <li class="menu-item"><a href="site.php"><i class="fa fa-home"></i>Início</a></li>
+            <li class="menu-item"><a href="index.php"><i class="fa fa-home"></i>Início</a></li>
             <li class="menu-item"><a href="manutencao.php"><i class="fa fa-wrench"></i>Manutenção</a></li>
             <li class="menu-item"><a href="otimizacao.php"><i class="fa fa-desktop"></i>Otimização</a></li>
             <li class="menu-item"><a href="artigos.php"><i class="fa fa-newspaper"></i>Artigos</a></li>
