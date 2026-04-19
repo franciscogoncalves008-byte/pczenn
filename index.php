9651dc2678d84adc1242880cc69f449d826f08f9
gasela <franciscogoncalves008@gmail.com>
2026-04-19 15:13:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if ($resultado) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PcZen</title>
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
