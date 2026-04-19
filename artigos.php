bed7e5978c113b3358f1082e106a2813a86764de
gasela <franciscogoncalves008@gmail.com>
2026-04-19 15:20:33 +0100

    Update artigos.php

--- artigos.php
+++ artigos.php
@@ -44,7 +44,7 @@ $nomes_categorias = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PcZen - Artigos</title>
-    <link rel="stylesheet" href="CSS/estilo.css">
+    <link rel="stylesheet" href="CSS/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
