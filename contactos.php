471c44d23f79aee8f51b0ca13997226dfd7c0ee7
gasela <franciscogoncalves008@gmail.com>
2026-04-19 15:19:06 +0100

    Update contactos.php

--- contactos.php
+++ contactos.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PcZen - Contactos</title>
-   <link rel="stylesheet" href="estilo.css">
+   <link rel="stylesheet" href="CSS/estilo.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
